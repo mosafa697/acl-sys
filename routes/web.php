5bc398e12544807092f3938b3008025f3f874324
mosafa697 <mosafa697@gmail.com>
2025-01-22 23:30:21 +0200

    feat: adding auth middleware to routes

--- routes/web.php
+++ routes/web.php
@@ -8,13 +8,15 @@ use App\Http\Controllers\UserPermissionController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::resource('users', UserController::class)->only(['index', 'create', 'store', 'destroy']);
-Route::resource('groups', GroupController::class);
-Route::post('/groups/{group}/assign-permissions', [GroupPermissionsController::class, 'assignPermissions'])->name('groups.assign-permissions');
-Route::resource('permissions', PermissionController::class);
-Route::get('/users/{user}/permissions/edit', [UserPermissionController::class, 'edit'])->name('user-permissions.edit');
-Route::put('/users/{user}/permissions', [UserPermissionController::class, 'update'])->name('user-permissions.update');
-
 Auth::routes();
 
+Route::middleware(['auth'])->group(function () {
+    Route::resource('users', UserController::class)->only(['index', 'create', 'store', 'destroy']);
+    Route::resource('groups', GroupController::class);
+    Route::post('/groups/{group}/assign-permissions', [GroupPermissionsController::class, 'assignPermissions'])->name('groups.assign-permissions');
+    Route::get('/users/{user}/permissions/edit', [UserPermissionController::class, 'edit'])->name('user-permissions.edit');
+    Route::put('/users/{user}/permissions', [UserPermissionController::class, 'update'])->name('user-permissions.update');
+});
+
+Route::resource('permissions', PermissionController::class);
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
